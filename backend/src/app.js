7b7d85d5b504f5754dcc5f7699f787e6d5518350
Tanujay Singh <tanujaysingh063@gmail.com>
2026-02-19 00:29:49 +0530

    db connection for phase 3

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 const searchRoutes = require('./routes/searchRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 const morgan = require("morgan");
+const pool  = require('./config/db');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(morgan('dev'));
+
+pool.query("SELECT NOW()")
+    .then(()=> console.log("DB Connected"))
+    .catch(console.error);
 
 app.use('/api', searchRoutes);
 
---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: "localhost",
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432
+});
+
+module.exports = pool;
